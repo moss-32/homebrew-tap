23723852eadeabb4016df055beb0778194a50275
Peter Bjorklund <piot@hotmail.com>
2026-01-31 20:27:27 +0100

    chore: moss-tools 0.1.1

--- Formula/moss_tools.rb
+++ Formula/moss_tools.rb
@@ -2,26 +2,27 @@ class MossTools < Formula
   desc "Moss Fantasy Console Tools"
   homepage "https://github.com/moss-32/tools"
 
-  version "0.1.0"
-
-  on_macos do
-    on_arm do
-      url "https://github.com/moss-32/tools/releases/download/v#{version}/moss-sprite-converter-#{version}-macos-arm64.tar.gz"
-      sha256 "cb12d5bf32c089a0374d93af8175c68ed1d94702070b9b93512fecec974703b3"
-    end
-
-    on_intel do
-      url "https://github.com/moss-32/tools/releases/download/v#{version}/moss-sprite-converter-#{version}-macos-x86_64.tar.gz"
-      sha256 "e2d47d64a4489223bb5fe4fdb7e29b2ac34ae12579381afe88bb079748735897"
-    end
-  end
+  version "0.1.1"
 
   on_linux do
     on_intel do
       url "https://github.com/moss-32/tools/releases/download/v#{version}/moss-sprite-converter-#{version}-linux-x86_64.tar.gz"
-      sha256 "ae66407b5ba9e4ebf24016d6b49ce491b242b10d8bd353d558c83ac58206994a"
+      sha256 "df3bc77372ae20aabac51cb7792935b43e35cb17f2ed5be36af362de132322a9"
     end
   end
+
+  on_macos do
+    on_arm do
+      url "https://github.com/moss-32/tools/releases/download/v#{version}/moss-sprite-converter-#{version}-macos-arm64.tar.gz"
+      sha256 "e477d657d432672378bb76c66183cc460717e2556e5948cce6b61bc8d97d68ca"
+    end
+
+    on_intel do
+      url "https://github.com/moss-32/tools/releases/download/v#{version}/moss-sprite-converter-#{version}-macos-x86_64.tar.gz"
+      sha256 "f1e51ab6e754c69f932b36d6a5bcd790c4072aef80bd57ba4e6b4c60fc9d74a6"
+    end
+  end
+
 
 def install
     os = OS.mac? ? "macos" : "linux"
