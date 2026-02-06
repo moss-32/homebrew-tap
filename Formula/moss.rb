a4df03e35525e22352b194f05ac2fe4c867d2d76
Peter Bjorklund <piot@hotmail.com>
2026-02-06 15:59:51 +0100

    chore: moss 0.1.10

--- Formula/moss.rb
+++ Formula/moss.rb
@@ -2,31 +2,31 @@ class Moss < Formula
   desc "Moss Fantasy Console"
   homepage "https://github.com/moss-32/moss"
 
-  version "0.1.9"
+  version "0.1.10"
 
 
   on_linux do
 
     on_arm do
       url "https://github.com/moss-32/moss/releases/download/v#{version}/moss-#{version}-linux-aarch64.tar.gz"
-      sha256 "3674d099d3b55870242455ae6dcec47ad09ac45eea40575ee34b95d4f16d4b9e"
+      sha256 "d1f829ae092f8e8f5e1d8877bef9995fdd9e3d64b5fc92f5fdbc418f2876b29e"
     end
 
     on_intel do
       url "https://github.com/moss-32/moss/releases/download/v#{version}/moss-#{version}-linux-x86_64.tar.gz"
-      sha256 "3674d099d3b55870242455ae6dcec47ad09ac45eea40575ee34b95d4f16d4b9e"
+      sha256 "251846d3080d9fbc77a43c1c00c3b9f4082278af42cd389de6a7fb720d8c1aa0"
     end
   end
 
   on_macos do
     on_arm do
       url "https://github.com/moss-32/moss/releases/download/v#{version}/moss-#{version}-macos-arm64.tar.gz"
-      sha256 "f8b75f362ffb83c1b61a20f726195002ebb59ba633482eddc2e10676496101fe"
+      sha256 "27c309fb25f8f2c96edf87ea00017d25a5a82898340bfcf7b3410ff65b591172"
     end
 
     on_intel do
       url "https://github.com/moss-32/moss/releases/download/v#{version}/moss-#{version}-macos-x86_64.tar.gz"
-      sha256 "b73bad42c7c34d554cb0d39ab3cb50f3528b6e93be618e7ea67480aa4fb74652"
+      sha256 "1acd779bbc0eb822ae4f36d9a1dbf19129778d449d99ae843a5bb085061b2313"
     end
   end
 
